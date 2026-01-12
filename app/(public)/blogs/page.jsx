90a27b37fce556b08273016ba8f3af49d953d4b0
faysal725 <faysal.dev@gmail.com>
2026-01-12 13:31:56 +0600

    case-study modified

--- app/(public)/blogs/page.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Blogs() {
-  return (
-    <div className="text-5xl">
-      Blogs page
-    </div>
-  );
-}
--- app/(public)/products/page.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Products() {
-  return (
-    <div className="text-5xl">
-      Products page
-    </div>
-  );
-}
